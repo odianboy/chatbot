8fe9708ce828224f17e3b3723ef3df073772090e
Anton <odianboy@mail.ru>
2021-01-07 00:10:38 +0300

    modified the bot and added tests

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@
 import logging
 import random
 
+import handlers
+
 try:
     import settings
 except ImportError:
@@ -15,26 +17,42 @@ log = logging.getLogger('bot')
 
 
 def configure_logging():
-
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(logging.Formatter(fmt='%(levelname)s %(message)s '))
     stream_handler.setLevel(logging.INFO)
     log.addHandler(stream_handler)
 
     file_handler = logging.FileHandler(filename='bot.log', encoding='UTF-8')
-    file_handler.setFormatter(logging.Formatter(fmt='%(asctime)s %(levelname)s %(message)s',  datefmt='%d-%m-%Y %H:%M'))
+    file_handler.setFormatter(logging.Formatter(fmt='%(asctime)s %(levelname)s %(message)s', datefmt='%d-%m-%Y %H:%M'))
     file_handler.setLevel(logging.DEBUG)
     log.addHandler(file_handler)
 
     log.setLevel(logging.DEBUG)
 
 
+class UserState:
+    """User state inside the script."""
+
+    def __init__(self, scenario_name, step_name, context=None):
+        self.scenario_name = scenario_name
+        self.step_name = step_name
+        self.context = context or {}
+
+
 class Bot:
     """
-    Echo bot for vk.com
+    Conference registration script "Conf" across vk.com.
+    Use python 3.9
 
-    Use python 3.7
+
+    Supports answers to questions about the date, venue and registration script:
+    - asks name
+    - asks email
+    - talks about a hasty registration
+
+    If the step is not passed, we ask a clarifying question until the step is passed.
     """
+
     def __init__(self, group_id, token):
         """
         :param group_id: group id from the group vk
@@ -42,11 +60,10 @@ class Bot:
         """
         self.group_id = group_id
         self.token = token
-
         self.vk = vk_api.VkApi(token=token)
         self.long_poller = VkBotLongPoll(self.vk, self.group_id)
-
         self.api = self.vk.get_api()
+        self.user_states = dict()  # user_id -> UserState
 
     def run(self):
         """Bot launch."""
@@ -57,20 +74,72 @@ class Bot:
                 log.exception('error in event handling')
 
     def on_event(self, event):
-        """Send message back, if it is text.
+        """
+        Send message back, if it is text.
 
         :param event: VkBotMessageEvent object
         :return: None
         """
-        if event.type == VkBotEventType.MESSAGE_NEW:
-            log.debug('send a message back')
-            self.api.messages.send(
-                message=event.object.text,
-                random_id=random.randint(0, 2 ** 20),
-                peer_id=event.object.peer_id,
-            )
-        else:
+
+        if event.type != VkBotEventType.MESSAGE_NEW:
             log.info('We are not yet able to handle events of this type. %s', event.type)
+            return
+
+        user_id = event.object.peer_id
+        text = event.object.text
+
+        if user_id in self.user_states:
+            text_to_send = self.continue_scenario(user_id, text)
+        else:
+            # search intent
+            for intent in settings.INTENTS:
+                log.debug(f'User gets {intent}')
+                if any(token in text.lower() for token in intent['tokens']):
+                    #   run intent
+                    if intent['answer']:
+                        text_to_send = intent['answer']
+                    else:
+                        text_to_send = self.start_scenario(user_id, intent['scenario'])
+                    break
+            else:
+                text_to_send = settings.DEFAULT_ANSWER
+
+        self.api.messages.send(
+            message=text_to_send,
+            random_id=random.randint(0, 2 ** 20),
+            peer_id=user_id,
+        )
+
+    def start_scenario(self, user_id, scenario_name):
+        scenario = settings.SCENARIOS[scenario_name]
+        first_step = scenario['first_step']
+        step = scenario['steps'][first_step]
+        self.user_states[user_id] = UserState(scenario_name=scenario_name, step_name=first_step)
+        text_to_send = step['text']
+        return text_to_send
+
+    def continue_scenario(self, user_id, text):
+        state = self.user_states[user_id]
+        steps = settings.SCENARIOS[state.scenario_name]['steps']
+        step = steps[state.step_name]
+
+        handler = getattr(handlers, step['handler'])
+        if handler(text=text, context=state.context):
+            # next step
+            next_step = steps[step['next_step']]
+            text_to_send = next_step['text'].format(**state.context)
+            if next_step['next_step']:
+                #   switch to next step
+                state.step_name = step['next_step']
+            else:
+                #   finish scenario
+                log.info('Зарегистрирован: {name} {email}'.format(**state.context))
+                self.user_states.pop(user_id)
+        else:
+            #  retry current step
+            text_to_send = step['failure_text'].format(**state.context)
+
+        return text_to_send
 
 
 if __name__ == '__main__':
--- tests.py
+++ tests.py
@@ -1,8 +1,10 @@
+from copy import deepcopy
 from unittest import TestCase
 from unittest.mock import patch, Mock, ANY
 
 from vk_api.bot_longpoll import VkBotMessageEvent
 
+import settings
 from bot import Bot
 
 
@@ -31,20 +33,48 @@ class Test1(TestCase):
                 bot.on_event.assert_any_call({})
                 assert bot.on_event.call_count == count
 
-    def test_on_event(self):
-        event = VkBotMessageEvent(raw=self.RAW_EVENT)
+    INPUTS = [
+        'Привет',
+        'А когда?',
+        'Где будет конференция?',
+        'Зарегистрируй меня',
+        'Вениамин',
+        'мой адрес email@email',
+        'email@email.ru',
+    ]
+    EXPECTED_OUTPUTS = [
+        settings.DEFAULT_ANSWER,
+        settings.INTENTS[0]["answer"],
+        settings.INTENTS[1]["answer"],
+        settings.SCENARIOS["registration"]["steps"]["step1"]["text"],
+        settings.SCENARIOS["registration"]["steps"]["step2"]["text"],
+        settings.SCENARIOS["registration"]["steps"]["step2"]["failure_text"],
+        settings.SCENARIOS["registration"]["steps"]["step3"]["text"].format(name='Вениамин', email='email@email.ru')
+    ]
 
+    def test_run_ok(self):
         send_mock = Mock()
+        api_mock = Mock()
+        api_mock.messages.send = send_mock
 
-        with patch('bot.vk_api.VkApi'):
-            with patch('bot.VkBotLongPoll'):
-                bot = Bot('', '')
-                bot.api.messages.send = send_mock
+        events = []
+        for input_text in self.INPUTS:
+            event = deepcopy(self.RAW_EVENT)
+            event['object']['text'] = input_text
+            events.append(VkBotMessageEvent(event))
 
-                bot.on_event(event)
+        long_poller_mock = Mock()
+        long_poller_mock.listen = Mock(return_value=events)
 
-        send_mock.assert_called_once_with(
-            message=self.RAW_EVENT['object']['text'],
-            random_id=ANY,
-            peer_id=self.RAW_EVENT['object']['peer_id']
-        )
+        with patch('bot.VkBotLongPoll', return_value=long_poller_mock):
+            bot = Bot('', '')
+            bot.api = api_mock
+            bot.run()
+
+        assert send_mock.call_count == len(self.INPUTS)
+
+        real_outputs = []
+        for call in send_mock.call_args_list:
+            args, kwargs = call
+            real_outputs.append(kwargs['message'])
+        assert real_outputs == self.EXPECTED_OUTPUTS
